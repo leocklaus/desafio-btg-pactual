c8ca53efdd0285e08b7383de012b0e78e624675e
leocklaus <leocklaus@gmail.com>
2024-07-17 17:43:54 -0300

    feat: project created

---
+++ src/test/java/io/github/leocklaus/desafiobtg/DesafioBtgPactualApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.leocklaus.desafiobtg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBtgPactualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
